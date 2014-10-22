5a8bffeaf28cd36958b877ac9791552613c35c8d
Zebraholic <rctavares@rogers.com>
2014-10-21 21:48:53 -0400

    Exercise 4 added.

---
+++ exercise4.rb
@@ -0,0 +1,14 @@
+(1..100).each do |number|
+  if number % 5 == 0 and number % 3 == 0
+        puts "Bitmaker Labs"
+  elsif number % 3 == 0
+        puts "Bitmaker"
+  elsif number % 5 == 0
+        puts "Labs"
+  else
+        puts number
+  end
+end
+
+
+
